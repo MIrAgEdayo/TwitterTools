4c86a62309b244d89e769894c3541f681c5d268f
MIrAgEdayo <45755582+MIrAgEdayo@users.noreply.github.com>
2023-12-10 00:27:14 +0900

    動作改善、コード整形

--- popup.js
+++ popup.js
@@ -1,11 +1,5 @@
-document.getElementById('btn_get').addEventListener('click', () => {
-    chrome.tabs.query( {active:true, currentWindow:true}, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, {message: 'getname'}, (content) => {
-            if(!content){
-                alert('Cannot Get! Try Reload First!');
-                return;
-            }
-            document.getElementById('title').value = content
-        });
-    });
+document.getElementById('btn_removeSpam').addEventListener('click', () => {
+	chrome.tabs.query( {active:true, currentWindow:true}, (tabs) => {
+		chrome.tabs.sendMessage(tabs[0].id, "removeSpam");
+	});
 });
